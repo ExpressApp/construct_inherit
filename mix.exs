e14d19b9ceffd4b72111f15a80ded712156c3138
Yuri Artemev <i@artemeff.com>
2018-09-07 08:35:42 +0300

    construct 2.0 required

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Construct.Inherit.MixProject do
 
   defp deps do
     [
-      {:construct, "~> 1.1"},
+      {:construct, "~> 2.0"},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:benchfella, "~> 0.3.5", only: :dev},
     ]
